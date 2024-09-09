fdb02942d965795d6ec38e3824914b3840448fc6
SimonThor12 <simon.lars.thor@gmail.com>
2024-09-09 10:07:41 +0200

    create: model folder with draft of tool model

---
+++ shar-tooly-backend/Models/Tool.cs
@@ -0,0 +1,9 @@
+
+public class Tool()
+{
+    public required string Id { get; set; }
+    public required string Model { get; set; }
+    public required string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+}
