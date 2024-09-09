a848e1e713f06ee463be9ac60c4b98d249a53c86
SimonThor12 <simon.lars.thor@gmail.com>
2024-09-09 10:03:34 +0200

    font import

--- shar-tooly-frontend/src/App.tsx
+++ shar-tooly-frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
 function App() {
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-primary">Shar tooly</h1>
+    <div className="flex flex-col items-center justify-center">
+      <img src="../public/shar-tooly-high-resolution-logo.png"></img>
+      <h1 className="text-primary font-semibold text-2xl">Welcome</h1>
     </div>
   );
 }
