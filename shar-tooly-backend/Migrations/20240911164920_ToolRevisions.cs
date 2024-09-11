25444c9d5ff16c62814bc5cce16ea1145bb0c336
SimonThor12 <simon.lars.thor@gmail.com>
2024-09-11 18:50:26 +0200

    migration

---
+++ shar-tooly-backend/Migrations/20240911164920_ToolRevisions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shar_tooly_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ToolRevisions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "Tools",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "Tools");
+        }
+    }
+}
