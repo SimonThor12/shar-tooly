e1b52de0bfbff3ef497db42d6ffd11368741d9fa
SimonThor12 <simon.lars.thor@gmail.com>
2024-09-11 11:18:14 +0200

    Username and password migration

---
+++ shar-tooly-backend/Migrations/20240911091513_UserRevisions5.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shar_tooly_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserRevisions5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Users");
+        }
+    }
+}
--- shar-tooly-backend/Models/User.cs
+++ shar-tooly-backend/Models/User.cs
@@ -5,4 +5,6 @@ public class User()
 {
     [Key]
     public required string Id { get; set; }
+    public required string Username { get; set; }
+    public required string Password { get; set; }
 }
